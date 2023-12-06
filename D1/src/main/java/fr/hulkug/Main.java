44bc415b758f842c8850f2798db645202e4ddbae
Hugo Poissonnier <hugo.poissonnier@epitech.eu>
2023-12-06 11:05:03 +0100

    Day 1 part 2

--- D1/src/main/java/fr/hulkug/Main.java
+++ D1/src/main/java/fr/hulkug/Main.java
@@ -6,8 +6,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Solver s1 = new Solver("input.txt");
-//        Solver s2 = new Solver("test1.txt");
-        s1.Solve();
+        Solver s2 = new Solver("test1.txt");
+        Solver s3 = new Solver("input.txt");
+//        s1.SolvePart1();
+//        s2.SolvePart2();
+        s3.SolvePart2();
     }
 
 }
--- D1/src/main/java/fr/hulkug/Solver.java
+++ D1/src/main/java/fr/hulkug/Solver.java
@@ -3,6 +3,9 @@ package fr.hulkug;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Solver {
     String fileName;
@@ -11,7 +14,7 @@ public class Solver {
         this.fileName = fileName;
     }
 
-    public void Solve() throws IOException {
+    public void SolvePart1() throws IOException {
         String file = getClass().getClassLoader().getResource(fileName).getFile();
         System.out.println(file);
         BufferedReader reader = new BufferedReader(new FileReader(file));
@@ -26,6 +29,41 @@ public class Solver {
                     number += currentChar;
                     break;
                 }
+            }
+            for ( int i = currentLine.length() - 1 ; i >= 0 ; i--){
+                char currentChar = currentLine.charAt(i);
+                if (Character.isDigit(currentChar)) {
+                    number += currentChar;
+                    break;
+                }
+            }
+            sum += Integer.parseInt(number);
+            System.out.println(number);
+            currentLine = reader.readLine();
+        }
+        System.out.println(sum);
+        reader.close();
+    }
+
+    public void SolvePart2() throws IOException{
+        String file = getClass().getClassLoader().getResource(fileName).getFile();
+        System.out.println(file);
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String currentLine = reader.readLine();
+        Map<String, String> numberLettered = Map.of("one", "on1e", "two", "tw2o", "three", "thr3ee", "four", "fo4ur", "five", "fi5ve", "six", "si6x", "seven", "sev7en", "eight", "eig8ht", "nine", "ni9ne");
+        int sum = 0;
+        while (currentLine != null){
+            String number = "";
+            for (Map.Entry<String, String> entry : numberLettered.entrySet()) {
+                currentLine = currentLine.replace(entry.getKey(), entry.getValue());
+            }
+            for ( int i = 0 ; i < currentLine.length() ; i++){
+                char currentChar = currentLine.charAt(i);
+                if (Character.isDigit(currentChar)) {
+                    number += currentChar;
+                    break;
+                }
+
             }
             for ( int i = currentLine.length() - 1 ; i >= 0 ; i--){
                 char currentChar = currentLine.charAt(i);
